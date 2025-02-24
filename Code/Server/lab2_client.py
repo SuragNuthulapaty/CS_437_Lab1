ce6d381d77db835a23a9f5999b58fff96a43f48f
SuragNuthulapaty <77648585+SuragNuthulapaty@users.noreply.github.com>
2025-02-23 21:27:13 -0600

    added all stuffs

--- Code/Server/lab2_client.py
+++ Code/Server/lab2_client.py
@@ -1,11 +1,4 @@
-import serial
-
-mac_serial_port = "/dev/cu.Bluetooth-Incoming-Port"  # Update this if necessary
-client = serial.Serial(mac_serial_port, baudrate=9600, timeout=1)
-
-client.write("Hello, Pi!\n".encode())
-
-response = client.readline().decode().strip()
-print(f"Response: {response}")
-
-client.close()
+from bluedot import BlueDot
+bd = BlueDot()
+bd.wait_for_press()
+print("You pressed the blue dot!")
