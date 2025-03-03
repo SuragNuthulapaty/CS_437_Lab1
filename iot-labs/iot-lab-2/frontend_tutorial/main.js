cd83ad28788f698e3a7483ad2e65a395b1faebac
elddc <emmachen2010@gmail.com>
2025-03-02 21:50:39 -0600

    Styles

--- iot-labs/iot-lab-2/frontend_tutorial/main.js
+++ iot-labs/iot-lab-2/frontend_tutorial/main.js
@@ -1,11 +1,13 @@
 const { app, BrowserWindow } = require("electron");
-
+try {
+    require('electron-reloader')(module);
+} catch {}
 function createWindow() {
     let win = new BrowserWindow({
         width: 800,
         height: 600,
         webPreferences: {
-            nodeIntegration: true, 
+            nodeIntegration: true,
             contextIsolation: false,
         }
     });
